5dbf3065f2219a765b3bc4ef73024dd5a727c8a4
Yeseul Lee <dev.yezlee@gmail.com>
2023-01-07 18:57:17 +0000

    The structure for backend settled

--- backend/src/controller/controller.js
+++ backend/src/controller/controller.js
@@ -1,25 +1,12 @@
 import * as express from "express";
-import { getMood, moodQuery } from "../model/query.js";
+import { getMoodQuery } from "../model/query.js";
 import db from "../db/connection.js";
 
 export const pingController = express.Router();
 
 pingController.get("/mood", async (req, res) => {
-  const data = await db(moodQuery);
+  const data = await db(getMoodQuery);
   console.log(data);
-
-  try {
-    data.length > 0
-      ? res.status(200).json(data)
-      : res.json({ message: "There's no data" });
-  } catch (e) {
-    console.error("There's an error", e.message);
-    next(e);
-  }
-});
-
-pingController.get("/mood2", async (req, res) => {
-  const data = await getMood();
 
   try {
     data.length > 0
--- backend/src/model/query.js
+++ backend/src/model/query.js
@@ -1,12 +1,5 @@
-import db from "../db/connection.js";
-
-export const moodQuery = `SELECT *
+export const getMoodQuery = `SELECT *
 FROM events
 WHERE care_recipient_id = 'ad3512a6-91b1-4d7d-a005-6f8764dd0111'
        AND event_type = 'mood_observation'
 ORDER BY timestamp DESC;`;
-
-export async function getMood() {
-  const mood = await db(moodQuery);
-  return mood;
-}
