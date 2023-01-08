ec1e7cf49396bc2bd4cc6367b0316f791349246c
Yeseul Lee <dev.yezlee@gmail.com>
2023-01-08 00:05:03 +0000

    Fetch data from deployed server

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,14 @@
 function App() {
+  // fetch("http://localhost:8080/mood")
+  //   .then((response) => response.json())
+  //   .then((data) => console.log(data))
+  //   .catch((e) => console.log("error"));
+
+  fetch("https://birdie-care-recipients.onrender.com/mood")
+    .then((response) => response.json())
+    .then((data) => console.log(data))
+    .catch((e) => console.log("error"));
+
   return (
     <div>
       <p>Hello, world!</p>
